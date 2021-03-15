c776f225b134039b55d1729612e6cd3a3affba36
Émile Lachance <1926353@cegepat.qc.ca>
2021-03-15 19:28:47 -0400

    Ajout d'un personnage
    Ajout d'un personnage

---
+++ Assets/Scripts/CamRotationer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotationer : MonoBehaviour
+{
+    public float lerpElasticity = 5f;    
+
+    // Pour empêcher la caméra de flippé si la souris va trop vers le haut/bas
+    float xAxisClamp = 0f;
+
+    // Le transform de la caméra
+    Transform mainCamTransform;
+
+    void Start()
+    {
+        // Vérouiller le curseur dans la fenètre
+        Cursor.lockState = CursorLockMode.Confined;
+
+        // Trouver la caméra
+        mainCamTransform = FindObjectOfType<Camera>().transform;
+
+        // Modifier la position du Cam Positioner
+        transform.position = mainCamTransform.position;
+        transform.rotation = mainCamTransform.rotation;
+
+        // Retirer le parent de la caméra
+        mainCamTransform.parent = null;
+    }
+
+    void Update()
+    {
+        // Calcul de la position et de la rotation
+        RotateCamPosition();
+    }
+
+    void FixedUpdate()
+    {        
+        // Positionner la caméra en FixedUpdate, car le personnage bouge en FixedUpdate
+        RepositionCamera();
+    }
+
+    void RotateCamPosition()
+    {
+        // Positions X et Y du curseur
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        xAxisClamp -= mouseY;
+
+        // Rotation du camPositioner et du character
+        Vector3 rotCam = transform.rotation.eulerAngles;
+
+        // Le positioner tourne en fonction de la position de la souris (x seulement)
+        rotCam.x -= mouseY;
+        rotCam.z = 0f;
+
+        // Empêcher la caméra de flip en rotation
+        if (xAxisClamp > 90f)
+        {
+            xAxisClamp = 90f;
+            rotCam.x = 90f;
+        } else if (xAxisClamp < -90f)
+        {
+            xAxisClamp = -90f;
+            rotCam.x = 270f;
+        }
+
+        // Appliquer les rotations
+        transform.rotation = Quaternion.Euler(rotCam);
+    }
+
+    /// <summary>
+    /// La caméra se déplace vers la position pour que ça soit smooth
+    /// </summary>
+    void RepositionCamera()
+    {
+        mainCamTransform.position = Vector3.Lerp(mainCamTransform.position, transform.position, lerpElasticity * Time.fixedDeltaTime);
+        mainCamTransform.rotation = Quaternion.Lerp(mainCamTransform.rotation, transform.rotation, lerpElasticity * Time.fixedDeltaTime);
+    }
+
+}
---
+++ Assets/Scripts/RbCharacterMovements.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RbCharacterMovements : MonoBehaviour
+{
+    public float speedWalking;
+    public float speedRunning;
+
+
+    public float jumpHeight = 1f;
+
+    // Transform de la position des pieds
+    public Transform feetPosition;
+
+    private float inputVertical;
+    private float inputHorizontal;    
+
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    private bool isGrounded = true;
+
+    private Animator animatorVanguard;
+
+    private float speed = 0.1f;
+
+    private float animationSpeed = 1;
+
+    private float lerpSpeed = 0.08f;
+
+    //Suis-je entrain de bouger ?
+    bool isMoving;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Assigner le Rigidbody
+        rb = GetComponent<Rigidbody>();
+
+        animatorVanguard = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vérifier si l'on touche le sol
+        isGrounded = Physics.CheckSphere(feetPosition.position, 0.15f, 1, QueryTriggerInteraction.Ignore);
+
+        // Vérifier les inputs du joueur
+        // Vertical (W, S et Joystick avant/arrière)
+        inputVertical = Input.GetAxis("Vertical");
+        // Horizontal (A, D et Joystick gauche/droite)
+        inputHorizontal = Input.GetAxis("Horizontal");
+
+        // Verifier les deadzones.
+
+        isMoving = Mathf.Abs(inputHorizontal) + Mathf.Abs(inputVertical) > 0f;
+
+        animatorVanguard.SetBool("isMoving", isMoving);
+        
+        // Animations ---------------------
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            // Courir
+            animationSpeed = Mathf.Lerp(animationSpeed, 2f, lerpSpeed);
+            speed = Mathf.Lerp(speed, speedRunning, lerpSpeed);
+
+        }
+        else
+        {
+            // Marche
+            animationSpeed = Mathf.Lerp(animationSpeed, 1f, lerpSpeed);
+            speed = Mathf.Lerp(speed, speedWalking, lerpSpeed);
+        }
+        animatorVanguard.SetFloat("Horizontal", inputHorizontal * animationSpeed);
+        animatorVanguard.SetFloat("Vertical", inputVertical * animationSpeed);
+
+        // ---------------------------------
+
+        // Vecteur de mouvements (Avant/arrière + Gauche/Droite)
+        moveDirection = transform.forward * inputVertical + transform.right * inputHorizontal;  
+        
+        // Sauter
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            animatorVanguard.SetTrigger("Jump");
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+
+        //Dance
+        if (Input.GetKeyDown(KeyCode.G) && !isMoving)
+        {
+            animatorVanguard.SetTrigger("Dance");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Déplacer le personnage selon le vecteur de direction
+        rb.MovePosition(rb.position + moveDirection.normalized * speed * Time.fixedDeltaTime);
+    }
+}
